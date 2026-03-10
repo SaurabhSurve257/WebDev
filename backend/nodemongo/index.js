0b98f8e446d0950a2c65cc431f87914325aea719
saurabhsurve257 <saurabhsurve257@gmail.com>
2026-03-10 22:52:40 +0530

    connect to mongodb

--- backend/nodemongo/index.js
+++ backend/nodemongo/index.js
@@ -1,18 +1,21 @@
-//const mongoose = require('mongoose');
-import mongoose from 'mongoose';
+// const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-// Replace with your MongoDB Atlas connection string
-const uri = 'mongodb+srv://saurabhsurve257_db_user:yMqJU95YURLUWGY2@cluster0.uktkeqp.mongodb.net/?appName=Cluster0';
+// MongoDB Atlas connection string
+const uri =
+  "mongodb://saurabhsurve257_db_user:yMqJU95YURLUWGY2@ac-iuviww8-shard-00-00.rklrmlf.mongodb.net:27017,ac-iuviww8-shard-00-01.rklrmlf.mongodb.net:27017,ac-iuviww8-shard-00-02.rklrmlf.mongodb.net:27017/?ssl=true&replicaSet=atlas-obuglc-shard-0&authSource=admin&appName=Cluster0";
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB Atlas');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB Atlas:', err);
-});
+// Function to connect DB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  }
+};
+
+// Call connection
+connectDB();
 
 // Add your application logic here
